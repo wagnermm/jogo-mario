3dc38c3eb492c9ae3ef7f2870a7771ceb6b348fd
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-03-31 14:16:55 -0300

    finalizando projeto

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const enemy = document.querySelector('.enemy')
+
+const jump = function() {
+
+    mario.classList.add('jump')
+
+    setTimeout(function() {
+        mario.classList.remove('jump')
+    },600)
+}
+
+const loop = setInterval(function(){
+
+    const pipePosition = pipe.offsetLeft
+    const enemyPosition = enemy.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './image/game-over.png'
+        mario.style.width = '80px'
+        mario.style.marginLeft = '50px'
+
+        enemy.style.animation = 'none'
+        enemy.style.left = `${enemyPosition}px`
+
+        clearInterval(loop)
+    }
+
+},10)
+
+document.addEventListener('keydown', jump)
