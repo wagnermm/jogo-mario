fa359240bd7d49b71f9a070d16c9be0dedb7dfbc
Wagner Miranda <miranda.wagnermarques@gmail.com>
2024-07-29 17:08:00 -0300

    altera obstaculo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const fireball = document.querySelector('.fireball');
 const gameOver = document.querySelector('.game-over');
 const gameWin = document.querySelector('.game-win');
 const refreshGame = document.querySelector('.refresh');
@@ -20,19 +20,19 @@ const jump = function() {
 
 document.addEventListener('click', jump);
 
-pipe.style.display = 'none';
+fireball.style.display = 'none';
 const start = function() {
     instructions.style.display = 'none';
     document.querySelector('.main').style.display = 'block';
-    pipe.style.display = 'block';
+    fireball.style.display = 'block';
 
     loop = setInterval(function() {
-        const pipePosition = pipe.offsetLeft;
+        const fireballPosition = fireball.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-        if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 70) {
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
+        if (fireballPosition <= 100 && fireballPosition > 0 && marioPosition < 70) {
+            fireball.style.animation = 'none';
+            fireball.style.left = `${fireballPosition}px`;
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
             mario.src = './image/game-over.png';
@@ -41,38 +41,42 @@ const start = function() {
             gameOver.style.visibility = "visible";
             document.querySelector('.game-over span').innerText = points;
             clearInterval(loop);
-        } else if (pipePosition <= 100 && pipePosition > 0 && marioPosition >= 70) {
+        } else if (fireballPosition <= 100 && fireballPosition > 0 && marioPosition >= 70) {
             points += 10;
             pointsElement.innerText = points;
 
-            if (points >= 2000) {
+            if (points >= 1000) {
                 container.style.background = 'url(image/background2.png)';
                 container.style.backgroundSize = 'cover';
                 container.style.backgroundRepeat = 'no-repeat';
-                pipe.style.animation='pipe-animation 2.5s infinite linear';
+                container.style.backgroundPosition = 'center center';
+                fireball.style.animation='fireball-animation 2.5s infinite easy-out';
             }
-            if (points >= 4000) {
+            if (points >= 2000) {
                 container.style.background = 'url(image/background3.jpg)';
                 container.style.backgroundSize = 'cover';
                 container.style.backgroundRepeat = 'no-repeat';
-                pipe.style.animation='pipe-animation 2s infinite linear';
+                container.style.backgroundPosition = 'center center';
+                fireball.style.animation='fireball-animation 2s infinite linear';
             }
-            if (points >= 8000) {
+            if (points >= 3000) {
                 container.style.background = 'url(image/background4.png)';
                 container.style.backgroundSize = 'cover';
                 container.style.backgroundRepeat = 'no-repeat';
-                pipe.style.animation='pipe-animation 1.5s infinite linear';
+                container.style.backgroundPosition = 'center center';
+                fireball.style.animation='fireball-animation 1.5s infinite linear';
             }
-            if (points >= 12000) {
+            if (points >= 4000) {
                 container.style.background = 'url(image/background5.jpg)';
                 container.style.backgroundSize = 'cover';
                 container.style.backgroundRepeat = 'no-repeat';
-                pipe.style.animation='none';
+                container.style.backgroundPosition = 'center center';
+                fireball.style.animation='none';
                 mario.src = './image/mario-win.png';
                 mario.style.width = '75px';
                 mario.style.marginLeft = '500px';
                 mario.style.bottom='0';
-                pipe.style.display = 'none';
+                fireball.style.display = 'none';
                 gameOver.style.visibility = "visible";
                 document.querySelector('.game-over span').innerText = points;
             }
